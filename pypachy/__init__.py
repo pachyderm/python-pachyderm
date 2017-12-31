f156b81e77bf05a9536b7d11e6f521a1669ea44d
Frank Hinek <frank.hinek@gmail.com>
2017-12-31 16:00:31 -0700

    Add support for bumpversion

--- pypachy/__init__.py
+++ pypachy/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+__version__ = '0.1.5'
+
 from .pfs_client import PfsClient, DIR, FILE, NONE, JSON, LINE
 from .pps_client import PpsClient, JOB_FAILURE, JOB_KILLED, JOB_RUNNING, JOB_STARTING, JOB_SUCCESS, FAILED, SUCCESS, \
     SKIPPED, POD_SUCCESS, POD_FAILED, POD_RUNNING, PIPELINE_FAILURE, PIPELINE_PAUSED, PIPELINE_RESTARTING, \
