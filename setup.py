9a123ab4a3552ee03a44171c3e83195d5b82635c
Diego Argueta <diego@goodrx.com>
2019-03-07 19:47:21 -0800

    Foil installation on unsupported Python versions

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -65,4 +64,5 @@ setup(
     test_suite='tests',
     tests_require=['pytest'],
     setup_requires=['pytest-runner'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )
