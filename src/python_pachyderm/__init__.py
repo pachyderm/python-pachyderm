b9a534af4b6320538e14ef3ae3ae11bba44a99af
Yusuf Simonson <simonson@gmail.com>
2019-04-03 17:39:22 -0400

    Import useful stuff at the module root

--- src/python_pachyderm/__init__.py
+++ src/python_pachyderm/__init__.py
@@ -4,11 +4,21 @@ from __future__ import absolute_import
 
 __version__ = '0.1.5'
 
-from .client.pfs.pfs_pb2 import DIR, FILE, NONE, JSON, LINE
+from .client.pfs.pfs_pb2 import DIR, FILE, NONE, JSON, LINE, \
+	Repo, Branch, BranchInfo, File, Block, Object, Tag, RepoInfo, \
+	RepoAuthInfo, Commit, CommitRange, CommitInfo, FileInfo, ByteRange, \
+	BlockRef, ObjectInfo, OverwriteIndex, PutFileRecord, PutFileRecords, \
+	ObjectIndex
 from .client.pps.pps_pb2 import JOB_FAILURE, JOB_KILLED, JOB_RUNNING, \
 	JOB_STARTING, JOB_SUCCESS, FAILED, SUCCESS, SKIPPED, POD_SUCCESS, \
 	POD_FAILED, POD_RUNNING, PIPELINE_FAILURE, PIPELINE_PAUSED, \
-	PIPELINE_RESTARTING, PIPELINE_RUNNING, PIPELINE_STARTING
+	PIPELINE_RESTARTING, PIPELINE_RUNNING, PIPELINE_STARTING, Secret, \
+	Transform, Egress, Job, Service, AtomInput, PFSInput, CronInput, \
+	GitInput, Input, JobInput, ParallelismSpec, HashtreeSpec, InputFile, \
+	Datum, DatumInfo, Aggregate, ProcessStats, AggregateProcessStats, \
+	WorkerStatus, ResourceSpec, GPUSpec, EtcdJobInfo, JobInfo, Worker, \
+	Pipeline, PipelineInput, EtcdPipelineInfo, PipelineInfo, LogMessage, \
+	ChunkSpec, SchedulingSpec
 
 from .pfs_client import PfsClient
 from .pps_client import PpsClient
