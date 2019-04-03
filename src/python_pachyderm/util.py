7791f710a4e28c8a63ea64bc810350a39552a086
Yusuf Simonson <simonson@gmail.com>
2019-04-03 17:13:05 -0400

    Removed deprecated functionality

--- src/python_pachyderm/util.py
+++ src/python_pachyderm/util.py
@@ -6,22 +6,6 @@ from python_pachyderm.client.pfs import pfs_pb2 as pfs_proto
 
 
 def get_address(host=None, port=None):
-    if host is None or port is None:
-        env_old_host = os.environ.get("PACHD_SERVICE_HOST")
-
-        if env_old_host is not None:
-            import warnings
-            warnings.warn("The environment variable `PACHD_SERVICE_HOST` is deprecated; either explicitly set the host, or use `PACHD_ADDRESS`", DeprecationWarning)
-            host = env_old_host
-
-    if port is None:
-        env_old_port = os.environ.get("PACHD_SERVICE_PORT_API_GRPC_PORT")
-
-        if env_old_port is not None:
-            import warnings
-            warnings.warn("The environment variable `PACHD_SERVICE_PORT_API_GRPC_PORT` is deprecated; either explicitly set the host, or use `PACHD_ADDRESS`", DeprecationWarning)
-            port = env_old_port
-
     if host is not None and port is not None:
         return "{}:{}".format(host, port)
     else:
