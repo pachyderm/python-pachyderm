e22525c31bbf846feab7d8fa6a51ad425ce17dc5
Yusuf Simonson <simonson@gmail.com>
2019-04-03 17:27:16 -0400

    Brought back root-level imports

--- src/python_pachyderm/__init__.py
+++ src/python_pachyderm/__init__.py
@@ -4,6 +4,12 @@ from __future__ import absolute_import
 
 __version__ = '0.1.5'
 
+from .client.pfs.pfs_pb2 import DIR, FILE, NONE, JSON, LINE
+from .client.pps.pps_pb2 import JOB_FAILURE, JOB_KILLED, JOB_RUNNING, \
+	JOB_STARTING, JOB_SUCCESS, FAILED, SUCCESS, SKIPPED, POD_SUCCESS, \
+	POD_FAILED, POD_RUNNING, PIPELINE_FAILURE, PIPELINE_PAUSED, \
+	PIPELINE_RESTARTING, PIPELINE_RUNNING, PIPELINE_STARTING
+
 from .pfs_client import PfsClient
 from .pps_client import PpsClient
 from grpc import RpcError
