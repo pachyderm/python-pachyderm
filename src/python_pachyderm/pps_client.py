952cf4c1e898fd5165155c85dbc5a6d3fff96c16
Christian Ladrón de Guevara Reyes <christian@datank.ai>
2019-07-22 16:21:06 -0500

    Adds TLS support
    Adds keyword arument root_certificates to PpsClient and PfsClient to
    create a secure client as described in https://grpc.github.io/grpc/python/_modules/grpc.html#secure_channel

--- src/python_pachyderm/pps_client.py
+++ src/python_pachyderm/pps_client.py
@@ -6,7 +6,7 @@ from python_pachyderm.util import commit_from, get_address, get_metadata
 
 
 class PpsClient(object):
-    def __init__(self, host=None, port=None, auth_token=None):
+    def __init__(self, host=None, port=None, auth_token=None, root_certificates=None):
         """
         Creates a client to connect to PPS.
 
@@ -14,11 +14,16 @@ class PpsClient(object):
         port: The port to connect to. Default is 30650.
         auth_token: The authentication token; used if authentication is
         enabled on the cluster. Default to `None`.
+        root_certificates:  The PEM-encoded root certificates as a byte string.
         """
 
         address = get_address(host, port)
         self.metadata = get_metadata(auth_token)
-        self.channel = grpc.grpc.insecure_channel(address)
+        if root_certificates:
+            ssl_credentials = grpc.grpc.ssl_channel_credentials(root_certificates=root_certificates)
+            self.channel = grpc.grpc.secure_channel(address, ssl_credentials)
+        else:
+            self.channel = grpc.grpc.insecure_channel(address)
         self.stub = grpc.APIStub(self.channel)
 
     def inspect_job(self, job_id, block_state=None, output_commit=None):
