a73cef8b360bab880421071a31c9f17f3038e080
Yusuf Simonson <simonson@gmail.com>
2019-09-16 15:13:49 -0400

    Reorg pfs/pps packages

--- src/python_pachyderm/__init__.py
+++ src/python_pachyderm/__init__.py
@@ -1,32 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .client.pfs.pfs_pb2 import DIR, FILE, NONE, JSON, LINE, CSV, \
-    Repo, Branch, BranchInfo, File, Block, Object, Tag, RepoInfo, \
-    RepoAuthInfo, Commit, CommitRange, CommitInfo, FileInfo, ByteRange, \
-    BlockRef, ObjectInfo, OverwriteIndex, PutFileRecord, PutFileRecords, \
-    ObjectIndex, CommitProvenance
-from .client.pfs.pfs_pb2 import \
-    STARTED as COMMIT_STATE_STARTED, \
-    READY as COMMIT_STATE_READY, \
-    FINISHED as COMMIT_STATE_FINISHED
-from .client.pps.pps_pb2 import JOB_FAILURE, JOB_KILLED, JOB_RUNNING, \
-    JOB_STARTING, JOB_SUCCESS, FAILED, SUCCESS, SKIPPED, POD_SUCCESS, \
-    POD_FAILED, POD_RUNNING, PIPELINE_FAILURE, PIPELINE_PAUSED, \
-    PIPELINE_RESTARTING, PIPELINE_RUNNING, PIPELINE_STARTING, Secret, \
-    Transform, Egress, Job, Service, PFSInput, CronInput, \
-    GitInput, Input, JobInput, ParallelismSpec, HashtreeSpec, InputFile, \
-    Datum, DatumInfo, Aggregate, ProcessStats, AggregateProcessStats, \
-    WorkerStatus, ResourceSpec, GPUSpec, JobInfo, Worker, Pipeline, \
-    PipelineInput, PipelineInfo, LogMessage, ChunkSpec, SchedulingSpec, \
-    ListDatumStreamResponse, PipelineInfos
-from .client.pps.pps_pb2 import \
-    FAILED as DATUM_FAILED, \
-    SUCCESS as DATUM_SUCCESS, \
-    SKIPPED as DATUM_SKIPPED, \
-    STARTING as DATUM_STARTING, \
-    RECOVERED as DATUM_RECOVERED
-
-from .pfs_client import PfsClient
-from .pps_client import PpsClient
+from .pfs import PfsClient
+from .pps import PpsClient
 from .util import get_remote_version
-from grpc import RpcError
