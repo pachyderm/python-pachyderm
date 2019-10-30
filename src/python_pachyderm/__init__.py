01061fb11aa53b94338b85276d58f638fceebf74
Yusuf Simonson <simonson@gmail.com>
2019-10-30 15:13:10 -0400

    Don't underscore prefix imported modules, since they're hidden via __all__ anyways

--- src/python_pachyderm/__init__.py
+++ src/python_pachyderm/__init__.py
@@ -1,6 +1,6 @@
-import string as _string
-import importlib as _importlib
-import enum as _enum
+import string as string
+import importlib as importlib
+import enum as enum
 from google.protobuf.internal.enum_type_wrapper import EnumTypeWrapper as _EnumTypeWrapper
 
 from .client import Client
@@ -23,9 +23,9 @@ def _import_protos(path):
     """
 
     g = globals()
-    module = _importlib.import_module(path)
-    uppercase_letters = set(_string.ascii_uppercase)
-    lowercase_letters = set(_string.ascii_lowercase)
+    module = importlib.import_module(path)
+    uppercase_letters = set(string.ascii_uppercase)
+    lowercase_letters = set(string.ascii_lowercase)
 
     for key in dir(module):
         if key[0] in uppercase_letters and any(c in lowercase_letters for c in key[1:]):
@@ -33,9 +33,9 @@ def _import_protos(path):
 
             if isinstance(value, _EnumTypeWrapper):
                 # Dynamically define an enum class that is exported
-                enum_values = _enum._EnumDict()
+                enum_values = enum._EnumDict()
                 enum_values.update(dict(value.items()))
-                enum_class = type(key, (_enum.IntEnum,), enum_values)
+                enum_class = type(key, (enum.IntEnum,), enum_values)
                 g[key] = enum_class
             else:
                 # Export the value
