3457b159ee8c5ee2bd09c95101da6032e5558f14
Yusuf Simonson <simonson@gmail.com>
2019-11-01 16:06:36 -0400

    Fixed tests

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -16,19 +16,19 @@ from tests import util
 def test_dump():
     client = python_pachyderm.Client()
     for b in client.dump():
-        assert isinstance(b, str)
+        assert isinstance(b, bytes)
         assert len(b) > 0
 
 
 def test_profile_cpu():
     client = python_pachyderm.Client()
     for b in client.profile_cpu(python_pachyderm.Duration(seconds=1)):
-        assert isinstance(b, str)
+        assert isinstance(b, bytes)
         assert len(b) > 0
 
 
 def test_binary():
     client = python_pachyderm.Client()
     for b in client.binary():
-        assert isinstance(b, str)
+        assert isinstance(b, bytes)
         assert len(b) > 0
