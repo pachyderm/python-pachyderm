f58b988c9db9967ce08a31fcac70c4475f423486
Yusuf Simonson <simonson@gmail.com>
2019-11-04 16:55:00 -0500

    Tests for enterprise

---
+++ tests/test_enterprise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Tests admin-related functionality"""
+
+import os
+
+import pytest
+
+import python_pachyderm
+from tests import util
+
+
+@util.skip_if_no_enterprise()
+def test_enterprise():
+    client = python_pachyderm.Client()
+    client.activate_enterprise(os.environ["PACH_PYTHON_ENTERPRISE_CODE"])
+    assert client.get_enterprise_state().state == python_pachyderm.State.ACTIVE
+    client.deactivate_enterprise()
