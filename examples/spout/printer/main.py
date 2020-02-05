8533c356a8756cee4377c683ba25d97d633d4a5c
Yusuf Simonson <simonson@gmail.com>
2020-02-05 11:20:41 -0500

    Repeatedly open spout

--- examples/spout/printer/main.py
+++ examples/spout/printer/main.py
@@ -4,10 +4,10 @@ import time
 import python_pachyderm
 
 def main():
-    with python_pachyderm.SpoutProducer() as spout:
-        while True:
-            spout.add_from_bytes("foo", b"bar")
-            time.sleep(1.0)
+    while True:
+        with python_pachyderm.SpoutProducer() as spout:
+            spout.add_from_bytes("foo", b"#")
+        time.sleep(5.0)
 
 if __name__ == "__main__":
     main()
