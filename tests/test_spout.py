e9dbdefaf1661897d855d2d34da459d5f30f0583
Yusuf Simonson <simonson@gmail.com>
2020-06-02 17:54:14 -0400

    Fix marker test

--- tests/test_spout.py
+++ tests/test_spout.py
@@ -7,10 +7,14 @@ import tempfile
 import python_pachyderm
 
 
-def test_spout_manager():
+def test_spout_manager_write():
     with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
         with python_pachyderm.SpoutManager(pfs_directory=d) as spout:
             spout.add_from_bytes("foo.txt", b"bar")
+
+def test_spout_manager_marker():
+    with tempfile.TemporaryDirectory(suffix="pachyderm",) as d:
+        with python_pachyderm.SpoutManager(pfs_directory=d, marker_filename="marker") as spout:
             spout.add_marker_from_bytes(b"marker")
             with spout.marker() as f:
                 assert f.read() == b"marker"
