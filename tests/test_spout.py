4c9bbca0cf34b1f241cb967161ab512674d2f187
Yusuf Simonson <simonson@gmail.com>
2020-06-03 08:16:47 -0400

    Just test writing

--- tests/test_spout.py
+++ tests/test_spout.py
@@ -7,14 +7,8 @@ import tempfile
 import python_pachyderm
 
 
-def test_spout_manager_write():
+def test_spout_manager():
     with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
-        with python_pachyderm.SpoutManager(pfs_directory=d) as spout:
-            spout.add_from_bytes("foo.txt", b"bar")
-
-def test_spout_manager_marker():
-    with tempfile.TemporaryDirectory(suffix="pachyderm",) as d:
         with python_pachyderm.SpoutManager(pfs_directory=d, marker_filename="marker") as spout:
+            spout.add_from_bytes("foo.txt", b"bar")
             spout.add_marker_from_bytes(b"marker")
-            with spout.marker() as f:
-                assert f.read() == b"marker"
