0bc07f339fb2ad2b5cd6cdc8a7c6d19ce7c3de4d
Matthew Steffen <msteffen@pachyderm.io>
2020-09-16 23:59:05 -0700

    Update tests for new exception handling

--- tests/test_spout.py
+++ tests/test_spout.py
@@ -6,6 +6,8 @@ import tempfile
 
 import pytest
 import python_pachyderm
+import tarfile
+from os.path import join
 
 
 def test_spout_manager():
@@ -15,9 +17,26 @@ def test_spout_manager():
         with manager.commit() as commit:
             commit.put_file_from_bytes("foo1.txt", b"bar1")
             commit.put_marker_from_bytes(b"marker1")
+
+        # Validate output
+        with tarfile.open(join(d, "out"), "r:") as t:
+            with t.extractfile("foo1.txt") as x:
+                assert x.read() == b"bar1"
+            with t.extractfile("marker") as x:
+                assert x.read() == b"marker1"
+
         with manager.commit() as commit:
             commit.put_file_from_bytes("foo2.txt", b"bar2")
             commit.put_marker_from_bytes(b"marker2")
+
+        # Validate output (note that because the most recent writes simply
+        # appended to the existing tar file, we must set ignore_zeros to ignore
+        # the extra padding before the second set of tar headers)
+        with tarfile.open(join(d, "out"), "r:", ignore_zeros=True) as t:
+            with t.extractfile("foo2.txt") as x:
+                assert x.read() == b"bar2"
+            with t.extractfile("marker") as x:
+                assert x.read() == b"marker2"
 
 def test_spout_manager_nested_commits():
     with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
@@ -35,8 +54,21 @@ def test_spout_manager_commit_state():
     with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
         manager = python_pachyderm.SpoutManager(pfs_directory=d, marker_filename="marker")
 
-        with pytest.raises(Exception):
-            with manager.commit() as commit:
-                raise Exception()
+        for _ in range(3):
+            with pytest.raises(Exception):
+                with manager.commit() as commit:
+                    raise Exception()
+            assert not manager._has_open_commit
 
-        assert manager._has_open_commit
+        # Now try to use the spout manager normally & confirm it still works
+        with manager.commit() as commit:
+            commit.put_file_from_bytes("foo1.txt", b"bar1")
+            commit.put_marker_from_bytes(b"marker1")
+
+        # Validate output. See note above re:ignore_zeros
+        with tarfile.open(join(d, "out"), "r:", ignore_zeros=True) as t:
+            with t.extractfile("foo1.txt") as x:
+                assert x.read() == b"bar1"
+            with t.extractfile("marker") as x:
+                assert x.read() == b"marker1"
+
