714568968464ed439edd93410cdf49dd85686c42
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-03-25 08:08:16 -0400

    Remove extract/restore methods

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -8,22 +8,6 @@ import python_pachyderm
 from tests import util
 
 
-def test_extract_restore():
-    client = python_pachyderm.Client()
-    ops = list(client.extract())
-    client.restore((python_pachyderm.RestoreRequest(op=op) for op in ops))
-
-
-@util.skip_if_below_pachyderm_version(1, 8, 8)
-def test_extract_pipeline_restore():
-    client = python_pachyderm.Client()
-    _, _, pipeline_name = util.create_test_pipeline(
-        client, "test_extract_pipeline_restore"
-    )
-    op = client.extract_pipeline(pipeline_name)
-    client.restore((python_pachyderm.RestoreRequest(op=op) for op in (op,)))
-
-
 def test_inspect_cluster():
     client = python_pachyderm.Client()
     res = client.inspect_cluster()
