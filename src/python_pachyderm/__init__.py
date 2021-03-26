90aa9e0d44a00b3b93c077f20c728a43b643fd0c
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-03-26 14:21:13 -0400

    PPS tests

--- src/python_pachyderm/__init__.py
+++ src/python_pachyderm/__init__.py
@@ -3,7 +3,7 @@ import importlib as _importlib
 import enum as _enum
 from google.protobuf.internal.enum_type_wrapper import EnumTypeWrapper as _EnumTypeWrapper
 
-from .mixin.pfs import PFSFile, PutFileClient
+from .mixin.pfs import PFSFile, ModifyFileClient
 from .client import Client, ConfigError, BadClusterDeploymentID
 from .spout import SpoutManager, SpoutCommit
 from .util import put_files, create_python_pipeline, parse_json_pipeline_spec, parse_dict_pipeline_spec
@@ -18,7 +18,7 @@ __all__ = [
     "put_files",
     "create_python_pipeline",
     "PFSFile",
-    "PutFileClient",
+    "ModifyFileClient",
     "parse_json_pipeline_spec",
     "parse_dict_pipeline_spec",
     "ConfigError",
