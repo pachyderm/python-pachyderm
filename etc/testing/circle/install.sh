f7fe4b44ad998500a965e35ae207103f93307f1d
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-06-29 12:36:30 -0400

    Get versions from version.json

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y -qq \
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
 if [ ! -f cached-deps/kubectl ] ; then
-    KUBECTL_VERSION=v1.19.2
+    KUBECTL_VERSION="v$(jq -r .kubernetes version.json)"
     curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
         chmod +x ./kubectl
         mv ./kubectl cached-deps/kubectl
@@ -34,13 +34,13 @@ fi
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort -V | tail -n1
 if [ ! -f cached-deps/minikube ] ; then
-    MINIKUBE_VERSION=v1.19.0 # If changed, also do etc/kube/start-minikube.sh
+    MINIKUBE_VERSION="v$(jq -r .minikube version.json)"
     curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
         chmod +x ./minikube
         mv ./minikube cached-deps/minikube
 fi
 
-export PACHYDERM_VERSION="2.0.0-alpha.23"
+export PACHYDERM_VERSION="$(jq -r .pachyderm version.json)"
 
 # Install Pachyderm
 curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v${PACHYDERM_VERSION}/pachctl_${PACHYDERM_VERSION}_amd64.deb
