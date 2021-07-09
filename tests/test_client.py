dcfb135ca5f837d5cd27cdee49ac73abce0ceea5
Sahith Malyala <sahith.malyala325@gmail.com>
2021-07-09 16:20:57 -0400

    Refactor new_from_config logic into Client default constructor (#299)
    * Add test for config lookup order
    
    * Refactor Client constructor + config lookup
    
    * Centralize tox + package dependencies
    
    * Add test for _parse_config + remove default client init test
    
    * Update syntax from deprecated setting

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,7 @@
 import io
+import os
+import json
+from pathlib import Path
 
 import pytest
 import python_pachyderm
@@ -7,9 +10,161 @@ from tests import util
 """Tests generic client functionality"""
 
 
-def test_client_init_with_default_host_port():
+def test_check_config_order(mocker):
+    env_config = json.loads(
+        """
+      {
+        "v2": {
+          "active_context": "local",
+          "contexts": {
+            "local": {
+              "pachd_address": "grpcs://172.17.0.6:30650",
+              "server_cas": "foo",
+              "session_token": "bar",
+              "active_transaction": "baz"
+            }
+          }
+        }
+      }
+    """
+    )
+    spout_config = json.loads(
+        """
+      {
+        "v2": {
+          "active_context": "local",
+          "contexts": {
+            "local": {
+              "pachd_address": "[::1]:80",
+              "server_cas": "foo",
+              "session_token": "bar",
+              "active_transaction": "baz"
+            }
+          }
+        }
+      }
+    """
+    )
+    local_config = json.loads(
+        """
+      {
+        "v2": {
+          "active_context": "local",
+          "contexts": {
+            "local": {
+              "port_forwarders": {
+                "pachd": 10101
+              }
+            }
+          }
+        }
+      }
+    """
+    )
+
+    mocker.patch(
+        "python_pachyderm.Client.check_pach_config_env_var", return_value=env_config
+    )
+    mocker.patch(
+        "python_pachyderm.Client.check_pach_config_spout",
+        return_value=spout_config,
+    )
+    mocker.patch(
+        "python_pachyderm.Client.check_pach_config_local",
+        return_value=local_config,
+    )
+
+    # Retrieves from env var config file
+    client = python_pachyderm.Client()
+    assert client.address == "172.17.0.6:30650"
+
+    # Retrieves from spout config file
+    mocker.patch("python_pachyderm.Client.check_pach_config_env_var", return_value=None)
+    client = python_pachyderm.Client()
+    assert client.address == "::1:80"
+
+    # Retrieves from local config file
+    mocker.patch("python_pachyderm.Client.check_pach_config_spout", return_value=None)
+    client = python_pachyderm.Client()
+    assert client.address == "localhost:10101"
+
+    # Executes default constructor behavior
+    mocker.patch("python_pachyderm.Client.check_pach_config_local", return_value=None)
     client = python_pachyderm.Client()
     assert client.address == "localhost:30650"
+
+
+def test_parse_config():
+    (
+        host,
+        port,
+        pachd_address,
+        auth_token,
+        root_certs,
+        transaction_id,
+        tls,
+    ) = python_pachyderm.Client._parse_config(
+        json.loads(
+            """
+        {
+          "v2": {
+            "active_context": "local",
+            "contexts": {
+              "local": {
+                "pachd_address": "grpcs://172.17.0.6:30650",
+                "server_cas": "foo",
+                "session_token": "bar",
+                "active_transaction": "baz"
+              }
+            }
+          }
+        }
+        """
+        )
+    )
+    assert host == "172.17.0.6"
+    assert port == 30650
+    assert pachd_address == "grpcs://172.17.0.6:30650"
+    assert auth_token == "bar"
+    assert root_certs == "foo"
+    assert transaction_id == "baz"
+    assert tls is True
+
+    (
+        host,
+        port,
+        pachd_address,
+        auth_token,
+        root_certs,
+        transaction_id,
+        tls,
+    ) = python_pachyderm.Client._parse_config(
+        json.loads(
+            """
+        {
+          "v2": {
+            "active_context": "local",
+            "contexts": {
+              "local": {
+                "port_forwarders": {
+                  "pachd": 10101
+                },
+                "server_cas": "foo",
+                "session_token": "bar"
+              }
+            }
+          }
+        }
+    """
+        )
+    )
+    assert host == "localhost"
+    assert port == 10101
+    assert pachd_address == "grpc://localhost:10101"
+    assert auth_token == "bar"
+    assert root_certs is None
+    assert transaction_id is None
+    assert tls is False
 
 
 def test_client_init_with_args():
@@ -159,7 +314,7 @@ def test_client_new_from_config():
     )
     assert client.address == "localhost:10101"
 
-    # empty context should default ot localhost:30650
+    # empty context should default to localhost:30650
     client = python_pachyderm.Client.new_from_config(
         config_file=io.StringIO(
             """
