fcaf2793e7148df527522a09f951b47e085fffc6
Sahith Malyala <sahith.malyala325@gmail.com>
2021-08-04 20:13:34 -0400

    Update spouts example (#309)

--- examples/spout/consumer/main.py
+++ examples/spout/consumer/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
 import datetime
 
 
 def main():
     with open("/pfs/producer/content", "r") as f:
-        print(datetime.datetime.now(), f.read())
+        with open("/pfs/out/results.txt", "w") as r:
+            r.write(str(datetime.datetime.now()) + "\n")
+            r.write(f.read())
 
 
 if __name__ == "__main__":
--- examples/spout/producer/main.py
+++ examples/spout/producer/main.py
@@ -5,22 +5,11 @@ import python_pachyderm
 
 
 def main():
-    # note: the marker filename is set when creating a pipeline, i.e. in
-    # `../main.py`.
-    manager = python_pachyderm.SpoutManager(marker_filename="marker")
+    manager = python_pachyderm.SpoutManager()
+    contents = b"#"
 
     while True:
-        try:
-            with manager.marker() as marker_file:
-                contents = marker_file.read()
-        except:
-            contents = b""
-
-        # append a # to the existing contents, pulled from the marker
-        contents += b"#"
-
         with manager.commit() as commit:
-            commit.put_marker_from_bytes(contents)
             commit.put_file_from_bytes("content", contents)
 
         time.sleep(5.0)
