782658d823fdd4ee4cfbe787cbca7982b8cbc6d9
Sahith Malyala <sahith.malyala325@gmail.com>
2021-08-04 17:12:48 -0700

    Update spouts test

--- tests/test_spout.py
+++ tests/test_spout.py
@@ -1,81 +1,52 @@
 #!/usr/bin/env python
 
-"""Tests of spout managers."""
-
-import tempfile
-
+"""Spout tests"""
+import time
 import pytest
 import python_pachyderm
-import tarfile
-from os.path import join
+from python_pachyderm.service import pps_proto, pfs_proto
 
 
-def test_spout_manager():
-    with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
-        manager = python_pachyderm.SpoutManager(
-            pfs_directory=d, marker_filename="marker"
-        )
+def test_create_spout():
+    client = python_pachyderm.Client()
+    client.delete_all()
 
-        with manager.commit() as commit:
-            commit.put_file_from_bytes("foo1.txt", b"bar1")
-            commit.put_marker_from_bytes(b"marker1")
+    client.create_pipeline(
+        pipeline_name="pipeline-create-spout",
+        transform=pps_proto.Transform(
+            cmd=["sh"],
+            image="alpine",
+        ),
+        spout=pps_proto.Spout(),
+    )
 
-        # Validate output
-        with tarfile.open(join(d, "out"), "r:") as t:
-            with t.extractfile("foo1.txt") as x:
-                assert x.read() == b"bar1"
-            with t.extractfile("marker") as x:
-                assert x.read() == b"marker1"
-
-        with manager.commit() as commit:
-            commit.put_file_from_bytes("foo2.txt", b"bar2")
-            commit.put_marker_from_bytes(b"marker2")
-
-        # Validate output (note that because the most recent writes simply
-        # appended to the existing tar file, we must set ignore_zeros to ignore
-        # the extra padding before the second set of tar headers)
-        with tarfile.open(join(d, "out"), "r:", ignore_zeros=True) as t:
-            with t.extractfile("foo2.txt") as x:
-                assert x.read() == b"bar2"
-            with t.extractfile("marker") as x:
-                assert x.read() == b"marker2"
+    assert len(list(client.list_pipeline())) == 1
 
 
-def test_spout_manager_nested_commits():
-    with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
-        manager = python_pachyderm.SpoutManager(
-            pfs_directory=d, marker_filename="marker"
-        )
+@pytest.mark.timeout(45)
+def test_spout_commit():
+    client = python_pachyderm.Client()
+    client.delete_all()
 
-        with manager.commit() as commit:
-            commit.put_file_from_bytes("foo1.txt", b"bar1")
-            commit.put_marker_from_bytes(b"marker1")
+    client.create_pipeline(
+        pipeline_name="pipeline-spout-commit",
+        transform=pps_proto.Transform(
+            cmd=["bash"],
+            stdin=[
+                "echo 'commit time' >> file.txt",
+                "pachctl put file pipeline-spout-commit@master:/file.txt -f file.txt",
+            ],
+        ),
+        spout=pps_proto.Spout(),
+    )
 
-            with pytest.raises(Exception):
-                with manager.commit() as commit:
-                    pass
+    c = client.subscribe_commit(
+        repo_name="pipeline-spout-commit",
+        branch="master",
+        state=pfs_proto.FINISHED,
+        origin_kind=pfs_proto.USER,
+    )
+    next(c)
 
-
-def test_spout_manager_commit_state():
-    with tempfile.TemporaryDirectory(suffix="pachyderm") as d:
-        manager = python_pachyderm.SpoutManager(
-            pfs_directory=d, marker_filename="marker"
-        )
-
-        for _ in range(3):
-            with pytest.raises(Exception):
-                with manager.commit() as commit:
-                    raise Exception()
-            assert not manager._has_open_commit
-
-        # Now try to use the spout manager normally & confirm it still works
-        with manager.commit() as commit:
-            commit.put_file_from_bytes("foo1.txt", b"bar1")
-            commit.put_marker_from_bytes(b"marker1")
-
-        # Validate output. See note above re:ignore_zeros
-        with tarfile.open(join(d, "out"), "r:", ignore_zeros=True) as t:
-            with t.extractfile("foo1.txt") as x:
-                assert x.read() == b"bar1"
-            with t.extractfile("marker") as x:
-                assert x.read() == b"marker1"
+    commit_infos = list(client.list_commit("pipeline-spout-commit"))
+    assert len(commit_infos) == 2
