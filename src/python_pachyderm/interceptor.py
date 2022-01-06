f5164b4a09dabc5b57575c4b75171bfb6978b9f4
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2022-01-06 15:19:59 -0800

    update type

--- src/python_pachyderm/interceptor.py
+++ src/python_pachyderm/interceptor.py
@@ -1,6 +1,5 @@
 from typing import Any, Callable, List, Tuple
 
-import grpc
 from grpc_interceptor import ClientCallDetails, ClientInterceptor
 
 MetadataType = List[Tuple[str, str]]
@@ -11,7 +10,7 @@ class MetadataClientInterceptor(ClientInterceptor):
         self.metadata = metadata
 
     def intercept(
-        self, method: Callable, request: Any, call_details: grpc.ClientCallDetails
+        self, method: Callable, request: Any, call_details: ClientCallDetails
     ):
         call_details_metadata = list(call_details.metadata or [])
         call_details_metadata.extend(self.metadata)
