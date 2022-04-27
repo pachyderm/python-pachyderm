f469c553f7635eea5f9fb995f176e12690543e4d
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2022-04-26 21:07:15 -0700

    deprecate python3.6 support

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -58,5 +58,5 @@ pip3 install tox
 
 # Install poetry
 curl -fsS -o install-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/install-poetry.py
-python3 install-poetry.py -y
+python3 install-poetry.py -y --version 1.1.8
 rm install-poetry.py
