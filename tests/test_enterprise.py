aff9542908393981687bfc2ebc9dce1d0641c761
Sahith <sahith.malyala@pachyderm.io>
2022-07-11 17:42:48 +0200

    Fix tests

--- tests/test_enterprise.py
+++ tests/test_enterprise.py
@@ -3,28 +3,38 @@
 """Tests admin-related functionality"""
 
 import os
+import pytest
 
 import python_pachyderm
 from python_pachyderm.service import enterprise_proto, identity_proto
 from tests import util
 
 
-@util.skip_if_no_enterprise()
-def test_enterprise():
-    client = python_pachyderm.Client()
-    client.delete_all_license()
+@pytest.fixture
+def client():
+    pc = python_pachyderm.Client()
+    pc.delete_all_license()
 
-    client.activate_license(os.environ["PACH_PYTHON_ENTERPRISE_CODE"])
-    client.add_cluster("localhost", "localhost:1650", secret="secret")
-    client.update_cluster("localhost", "localhost:1650", "localhost:16650")
-    client.activate_enterprise("localhost:1650", "localhost", "secret")
+    pc.activate_license(os.environ["PACH_PYTHON_ENTERPRISE_CODE"])
+    pc.add_cluster("localhost", "localhost:1650", secret="secret")
+    pc.update_cluster("localhost", "localhost:1650", "localhost:16650")
+    pc.activate_enterprise("localhost:1650", "localhost", "secret")
 
-    client.auth_token = "iamroot"
-    client.activate_auth(client.auth_token)
-    client.set_identity_server_config(
+    pc.auth_token = "iamroot"
+    pc.activate_auth(pc.auth_token)
+    pc.set_identity_server_config(
         config=identity_proto.IdentityServerConfig(issuer="http://localhost:1658")
     )
 
+    yield pc
+
+    pc.delete_cluster("localhost")
+    pc.deactivate_enterprise()
+    pc.delete_all()
+
+
+@util.skip_if_no_enterprise()
+def test_enterprise(client):
     assert len(client.list_clusters()) == len(client.list_user_clusters())
     assert client.get_enterprise_state().state == enterprise_proto.State.ACTIVE
     assert (
@@ -35,7 +45,3 @@ def test_enterprise():
         client.get_pause_status().status
         == enterprise_proto.PauseStatusResponse.PauseStatus.UNPAUSED
     )
-
-    client.delete_cluster("localhost")
-    client.deactivate_enterprise()
-    client.delete_all()
